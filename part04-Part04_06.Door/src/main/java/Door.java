27975c4a0a0a9bd3b89c2e87c2674e2fcc5bdd9c
SphinxBlacc <86077071+SphinxBlacc@users.noreply.github.com>
2021-06-26 23:56:43 +0530

    Add files via upload

---
+++ part04-Part04_06.Door/src/main/java/Door.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author sudha_000
+ */
+public class Door {
+    
+    private String Door;
+    
+    public Door() {
+    
+    }
+    
+    public void knock() {
+        System.out.println("Who's there?");
+    }
+}
